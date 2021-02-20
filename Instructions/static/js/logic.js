f217a0565d1b95d672ad612ab447e08d78fb5992
markrotil <mark.rotil@hotmail.com>
2021-02-20 01:42:16 -0500

    started code

---
+++ Instructions/static/js/logic.js
@@ -0,0 +1,14 @@
+var myMap = L.map("mapid", {
+    center: [43.6532, -79.3832],
+    zoom: 13
+})
+
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+}).addTo(myMap);
