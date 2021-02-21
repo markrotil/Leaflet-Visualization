ca2eb06094b5503455967e73b2a1d8154fbb7e6e
markrotil <mark.rotil@hotmail.com>
2021-02-20 19:57:17 -0500

    added layers

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -3,12 +3,31 @@ var myMap = L.map("mapid", {
     zoom: 13
 })
 
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Sattelite Layer
+var satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox://styles/mapbox/satellite-v9',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
 }).addTo(myMap);
+
+// Streetmap Layer
+var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox://styles/mapbox/light-v10",
+    accessToken: API_KEY
+});
+
+//Outdoor Layer
+var outdoor = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox://styles/mapbox/outdoors-v11",
+    accessToken: API_KEY
+});
+
